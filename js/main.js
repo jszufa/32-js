6c1613d60722833ea5aba324c252089d1dfcbbc3
jszufa <jerzy.szufa@gmail.com>
2022-11-28 15:29:06 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+let num1 = 2;
+
+let num2 = 5;
+
+if (num1 > num2){
+    console.log(num1);
+}
+    else if (num1 < num2){
+    console.log(num2);
+}
+    else {
+    console.log('Żadna z liczb nie jest większa od drugiej');
+};
+
+
+
+/* function bigger(x, y){
+    if (x > y){
+        return x;
+    }
+        else if (x==y){
+        return 'Żadna z liczb nie jest większa od drugiej';
+    }
+        else {
+        return y;
+    }
+};
+
+console.log(bigger(num1, num2)); */
